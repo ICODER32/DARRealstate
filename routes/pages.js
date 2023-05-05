0f159da1edec472d993964106af3580550481dce
Ibtisam Anwar <ibtisamanwar008@gmail.com>
2023-05-05 08:10:12 +0500

    booking oages added

--- routes/pages.js
+++ routes/pages.js
@@ -4,16 +4,29 @@ const Land = require("../models/Land");
 const Apartment = require("../models/Apartment");
 const Villa = require("../models/Villa");
 
-router.get("/", (req, res) => {
+router.get("/", async (req, res) => {
   const user_id = req.cookies.user_id;
   const role = req.cookies.role;
-  if (req.cookies.user_id) {
-    return res.render("index", {
-      user_id,
-      role,
+
+  try {
+    const villas = await Villa.find({});
+    const apartments = await Apartment.find({});
+    const lands = await Land.find({});
+    if (req.cookies.user_id) {
+      return res.render("index", {
+        user_id,
+        role,
+        apartments,
+        lands,
+        villas,
+      });
+    } else {
+      return res.redirect("/login");
+    }
+  } catch (error) {
+    return res.render("error", {
+      error: "Something went Wrong try Reloading",
     });
-  } else {
-    return res.redirect("/login");
   }
 });
 router.get("/login", (req, res) => {
@@ -37,6 +50,16 @@ router.get("/addProperty", (req, res) => {
     user_id,
     role,
   });
+});
+router.get("/land/:id", (req, res) => {
+  console.log("land");
+});
+router.get("/apartment/:id", (req, res) => {
+  console.log("APARTMENT");
+});
+
+router.get("/villa/:id", (req, res) => {
+  console.log("VILLA");
 });
 
 module.exports = router;
